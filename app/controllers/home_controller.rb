5db0b2db754c80d43616689b690eaa1be6f5829b
KubaHodi <jakhod2007@gmail.com>
2025-08-19 15:43:18 +0200

    Installed redis via snap, broadcast after commit for users and rooms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all_except(session[:user_id])
+    @users = User.all_except(session[:user_id]).count
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   scope :all_except, ->(user) { where.not(id: user) } #zakres umożliwiający pobranie wszystkich użytkowników z wyjątkiem samego siebie
   has_secure_password
   has_one_attached :avatar
-  def self.ransackable_attributes(auth_object = nil)
-    %w[ username ]
-  end
+  after_create_commit { broadcast_append_to "users" }
+
+    def self.ransackable_attributes(auth_object = nil)
+      %w[ username ]
+    end
 end
