7363780ac76b2ebd7fd8e8aa6bfd2fa1d632ead3
KubaHodi <jakhod2007@gmail.com>
2025-09-12 12:23:52 +0200

    WIP system test, small fixes

--- spec/support/system_helpers.rb
+++ spec/support/system_helpers.rb
@@ -5,4 +5,12 @@ module SystemHelpers
         fill_in "Password",	with: password
         click_button "Login"  
     end
+
+    def system_register(user, password: "123")
+        visit register_path
+        fill_in "Username", with: user
+        fill_in "Password", with: password
+        fill_in "Password confirmation", with: password
+        click_button "Create User"
+    end
 end
