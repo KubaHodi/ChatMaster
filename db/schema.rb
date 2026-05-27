1aecbaa9b497a91648fbfec2758584b4b43216b0
KubaHodi <jakhod2007@gmail.com>
2026-05-28 00:08:49 +0200

    Add solid cable messages migration:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_25_091911) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_27_220558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -88,6 +88,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_091911) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
