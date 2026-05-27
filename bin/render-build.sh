553da3b0e52e02d7404dcb3c3983d983872f0ab8
KubaHodi <jakhod2007@gmail.com>
2026-05-28 01:23:56 +0200

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,17 @@
 
 set -o errexit
 
+echo "=== BUNDLE INSTALL ==="
 bundle install
 
+echo "=== DB PREPARE ==="
 bin/rails db:prepare
 
+echo "=== CHECK SOLID QUEUE ==="
 bin/rails runner 'abort("Missing solid_queue_jobs") unless SolidQueue::Job.connection.data_source_exists?("solid_queue_jobs"); puts "Solid Queue tables OK"'
 
+echo "=== ASSETS PRECOMPILE ==="
 bin/rails assets:precompile
+
+echo "=== ASSETS CLEAN ==="
 bin/rails assets:clean
