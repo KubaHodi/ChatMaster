7d57359c19549a8b4265c54853a1a79ce4d72047
KubaHodi <jakhod2007@gmail.com>
2026-05-28 00:37:16 +0200

    fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Exit on error
 set -o errexit
 
 bundle install
+
+bin/rails db:prepare
+
 bin/rails assets:precompile
 bin/rails assets:clean
-
-# If you have a paid instance type, we recommend moving
-# database migrations like this one from the build command
-# to the pre-deploy command:
-bin/rails db:migrate
