5da62156bf93157a93284548cc8d3d41e8b0e13f
KubaHodi <jakhod2007@gmail.com>
2026-05-28 00:12:20 +0200

    Add solid cable messages migration:

--- db/migrate/20260527220558_create_solid_cable_messages.rb
+++ db/migrate/20260527220558_create_solid_cable_messages.rb
@@ -1,7 +1,13 @@
 class CreateSolidCableMessages < ActiveRecord::Migration[8.0]
   def change
     create_table :solid_cable_messages do |t|
-      t.timestamps
+      t.binary :channel, null: false
+      t.binary :payload, null: false
+      t.datetime :created_at, null: false
+      t.bigint :channel_hash, null: false
     end
+
+    add_index :solid_cable_messages, :channel_hash
+    add_index :solid_cable_messages, :created_at
   end
 end
